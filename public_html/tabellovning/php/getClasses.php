bea69d4d3b7c647f20db053e55c4e5783b41ea79
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2022-05-19 13:20:23 +0300

    Redigerade getClasses.php

--- public_html/tabellovning/php/getClasses.php
+++ public_html/tabellovning/php/getClasses.php
@@ -1,16 +1,24 @@
 <?php
+declare (strict_types=1);
+
 require_once "funktioner.php";
+
+if($_SERVER['REQUEST_METHOD']!=="POST") {
+    $error = new stdClass();
+    $error -> error = ["Felaktigt anrop", "Sidan ska anropas med POST"];
+    skickaSvar($error, 405);
+}
 
 $db = kopplaDatabas();
 
-$sql="SELECT klass FROM klasser";
+$sql="SELECT ID, klass FROM klasser";
 $stmt = $db->query($sql);
 $resultat = $stmt->fetchAll();
 
 $out = new stdClass();
 $out -> classes=[];
 foreach($resultat as $row) {
-    $out->classes[]=['klass'=>$row['klass']];
+    $out->classes[]=['ID'=>$row['ID'], 'klass'=>$row['klass']];
 }
 
 skickaSvar($out, 200);
