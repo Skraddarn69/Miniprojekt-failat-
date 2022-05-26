db194005db0544c810b9211ddb1ffe117a75c876
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2022-05-26 13:42:05 +0300

    Redigerade getMenus.js

--- public_html/tabellovning/js/getMenus.js
+++ public_html/tabellovning/js/getMenus.js
@@ -1,11 +1,31 @@
-function appendClasses(userType) {
+function getClasses(userType, teacherID) {
     if(userType===1) {
-        fetch('../php/getClasses.php')
+        fetch('../php/getClasses.php?anvandarTyp=' + userType)
         .then(function(response) {
             if(response.status == 200) {
                 return response.json();
             }
         })
-        
-    } 
+        .then(function(data) {
+            appendClasses(data);
+        })
+    } else {
+        fetch('../php/getClasses.php?anvandarTyp=' + userType + '&lararID=' + teacherID)
+        .then(function(response) {
+            if(response.status == 200) {
+                return response.json();
+            }
+        })
+        .then(function(data) {
+            appendClasses(data);
+        })
+    }
+}
+
+function appendClasses(data) {
+    for(let i=0;i<data.classes.length;i++) {
+        let cells = document.getElementsByTagName("td");
+        cells.item(i).id = data.classes(i).ID;
+        cells.item(i).innerHTML = data.classes(i).klass;
+    }
 }
