e3b0fbda883107a56edbc65f9e260085a7e6f5f8
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2022-05-26 13:15:02 +0300

    Rediger getClasses.php

--- public_html/tabellovning/php/getClasses.php
+++ public_html/tabellovning/php/getClasses.php
@@ -9,27 +9,50 @@ if($_SERVER['REQUEST_METHOD']!=="POST") {
     skickaSvar($error, 405);
 }
 
-if(!isset($_POST['lararID'])) {
+if(!isset($_POST['anvandarTyp'])) {
     $error = new stdClass();
-    $error -> error = ["Felaktigt anrop", "Parametern 'lararID' saknas"];
+    $error -> error = ["Felaktigt anrop", "Parametern 'anvandarTyp' saknas"];
     skickaSvar($error, 400);
 }
 
-$lararID = filter_input(INPUT_POST, 'lararID', FILTER_SANITIZE_NUMBER_INT);
+$anvandarTyp = filter_input(INPUT_POST, 'anvandarTyp', FILTER_SANITIZE_NUMBER_INT);
 $unwanted = "+\-";
-$lararID = trim($lararID, $unwanted);
-if($lararID==="") {
+$anvandarTyp = trim($anvandarTyp, $unwanted);
+
+if($anvandarTyp==="") {
     $error = new stdClass();
-    $error -> error = ["Felaktigt anrop", "'lararID' får inte vara tom"];
+    $error -> error = ["Felaktigt anrop", "Parametern 'anvandarTyp' får endast vara 1 eller 2 och inte vara tom"];
     skickaSvar($error, 400);
 }
 
-$db = kopplaDatabas();
+if($anvandarTyp==2) {
+    if(!isset($_POST['lararID'])) {
+        $error = new stdClass();
+        $error -> error = ["Felaktigt anrop", "Parametern 'lararID' saknas"];
+        skickaSvar($error, 400);
+    }
 
-$sql="SELECT ID, klass FROM klasser WHERE lararID=:lararID";
-$stmt = $db -> prepare($sql);
-$stmt -> execute(['lararID'=>$lararID]);
-$resultat = $stmt->fetchAll();
+    $lararID = filter_input(INPUT_POST, 'lararID', FILTER_SANITIZE_NUMBER_INT);
+    $lararID = trim($lararID, $unwanted);
+
+    if($lararID==="") {
+        $error = new stdClass();
+        $error -> error = ["Felaktigt anrop", "'lararID' får inte vara tom"];
+        skickaSvar($error, 400);
+    }
+
+    $db = kopplaDatabas();
+
+    $sql="SELECT ID, klass FROM klasser WHERE lararID=:lararID";
+    $stmt = $db -> prepare($sql);
+    $stmt -> execute(['lararID'=>$lararID]);
+} elseif($anvandarTyp==1) {
+    $db = kopplaDatabas();
+
+    $sql="SELECT ID, klass FROM klasser";
+    $stmt = $db -> query($sql);
+}
+    $resultat = $stmt->fetchAll();
 
 $out = new stdClass();
 $out -> classes=[];
