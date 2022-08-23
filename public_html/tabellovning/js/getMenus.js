b8c69b1d5c76c0f2e69af7b9867365be00c1cc1d
DESKTOP-TAPJHRS\Linus Dahlblom <46130@gymnasium.ax>
2022-08-23 14:41:38 +0300

    Fixade funktionen appendNav och skapade css-fil

--- public_html/tabellovning/js/getMenus.js
+++ public_html/tabellovning/js/getMenus.js
@@ -1,6 +1,5 @@
 // Hämtar klasser
-function getClasses(page, teacherID) {
-    
+function getClasses(page, teacherID) {   
     if((teacherID===null)) {
         fetch('http://localhost/Miniprojekt/public_html/tabellovning/php/getClasses.php?teacherID=' +teacherID +'page=' +page)
         .then(function(response) {
@@ -27,20 +26,52 @@ function getClasses(page, teacherID) {
 // Fyller tabell med klasser
 function appendClasses(data, page) {
     let classes = data.classes;
-    let pages;
+    let classCount = Object.keys(classes).length;
+    let pages = data.pages;
     let table = document.getElementById("menu");
     let cells = table.getElementsByTagName("td");
-    
-    if (pages > 1) {
-        pages = data.pages; 
-        appendNav(page, pages);
-    }
 
     for(let i=0;i<cells.length;i++) {
+        cells[i].innerHTML = "";
+    }
+
+    appendNavClasses(page, pages);
+    
+    for(let i=0;i<classCount;i++) {
         cells.item(i).innerHTML = classes[i].klass;
+        cells.item(i).onclick = function() {console.log(cells.item(i).innerHTML)};
     }
 }
 
-function appendNav(page, pages) {
+function appendNavClasses(page, pages) {
+    let prev = document.getElementById("previous");
+    let next = document.getElementById("next");
     
+    if(page!=1) {
+        prev.style.color = "initial";
+        prev.onclick = function() {getClasses(page-1)};
+    } else {
+        prev.style.color = "grey";
+        prev.onclick = "";
+    }
+
+    if(page!=pages) {
+        next.style.color = "initial";
+        next.onclick = function() {getClasses(page+1)};
+    } else {
+        next.style.color = "grey"
+        next.onclick = "";
+    }
+}
+
+function getStudents(page, classId) {
+        fetch('http://localhost/Miniprojekt/public_html/tabellovning/php/getStudents.php?page=' +page)
+        .then(function(response) {
+            if(response.status == 200) {
+                return response.json();
+            }
+        })
+        .then(function(data) {
+            appendClasses(data, page);
+        })
 }
